19644505025e5e613264cfc1bbb3de1a616e62c1
watsonkh <watsonkh@vcu.edu>
2022-05-31 17:18:02 -0400

    Warning if 0 nodes are captured when creating a BC

--- BoundaryConditions.jl
+++ BoundaryConditions.jl
@@ -43,6 +43,10 @@ function get_nodes_within_volume(nodes, volume)
             push!(n, node)
         end
     end
+
+    if size(n)[1] == 0
+        println("WARNING, CAPTURED VOLUME WITH 0 NODES!!!!")
+    end
     return n
 end
 
