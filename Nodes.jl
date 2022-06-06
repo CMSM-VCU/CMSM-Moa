7884da583a6fff33b87b188d0bdf3b096ee670a6
watsonkh <watsonkh@vcu.edu>
2022-06-06 18:10:30 -0400

    Adaptive dynamic relaxation

--- Nodes.jl
+++ Nodes.jl
@@ -9,14 +9,46 @@ mutable struct Node <: AbstractNode
     position::MVector{3, Float64}
     displacement::MVector{3, Float64}
     velocity::MVector{3, Float64}
+    oldAverageVelocity::MVector{3, Float64}
+    stableMass::Float64
+
     @atomic force::MVector{3, Float64}
+    oldForce::MVector{3, Float64}
+
     volume::Float64
     material::Materials.AbstractMaterial
 end
 
 # Some constructors
-Node(x::Float64, y::Float64, z::Float64, m::Materials.AbstractMaterial, grid_spacing::Float64) = Node([x,y,z], [0,0,0], [0,0,0], [0,0,0], grid_spacing^3, m)
-Node(pos::MVector{3, Float64}, m::Materials.AbstractMaterial, grid_spacing::Float64) = Node(pos, [0,0,0], [0,0,0], [0,0,0], grid_spacing^3, m)
+Node(x::Float64, y::Float64, z::Float64, m::Materials.AbstractMaterial, grid_spacing::Float64) = Node(
+    [x,y,z],
+    [0,0,0],
+    [0,0,0],
+    [0,0,0],
+    0,
+    [0,0,0],
+    [0,0,0],
+    grid_spacing^3,
+    m)
 
+Node(pos::MVector{3, Float64}, m::Materials.AbstractMaterial, grid_spacing::Float64) = Node(
+    pos,
+    [0,0,0],
+    [0,0,0],
+    [0,0,0],
+    0,
+    [0,0,0],
+    [0,0,0],
+    grid_spacing^3,
+    m)
+
+
+function mass(node::Node)
+    return node.volume * node.material.density
+end
+
+function stableMass(node::Node, dt, horizon, gridspacing)
+    0.25 * dt^2 * 4.0/3.0 * pi * horizon^3 * node.material.bond_constant / gridspacing
+end
 
 end
--- TimeIntegration.jl
+++ TimeIntegration.jl
@@ -52,4 +52,80 @@ function dynamic_integration(nodes, bonds, bcs, dt)
 
 end
 
+function adr(nodes::Vector{Nodes.AbstractNode},
+                bonds::Vector{Bonds.AbstractBond},
+                bcs::Vector{BoundaryConditions.AbstractBoundaryCondition},
+                dt::Float64,
+                stale::Bool)
+    
+
+    Threads.@threads for node in nodes
+        @atomic node.force = zeros(3)
+    end
+    
+    # Apply bond force to nodes
+    Threads.@threads for bond in bonds
+        Bonds.apply_force(bond)
+    end
+
+
+    # Calculates factor
+    cn = 0.0
+    cn1 = 0.0
+    cn2 = 0.0
+
+    for node in nodes
+        # Need old force and old average velocty
+        if node.oldAverageVelocity[1] != 0
+            cn1 -= (node.displacement[1]^2 * (node.force[1] - node.oldForce[1])) / (dt * node.oldAverageVelocity[1] * node.stableMass)
+        end
+        if node.oldAverageVelocity[2] != 0
+            cn1 -= (node.displacement[2]^2 * (node.force[2] - node.oldForce[2])) / (dt * node.oldAverageVelocity[2] * node.stableMass)
+        end
+        if node.oldAverageVelocity[3] != 0
+            cn1 -= (node.displacement[3]^2 * (node.force[3] - node.oldForce[3])) / (dt * node.oldAverageVelocity[3] * node.stableMass)
+        end
+        # cn1 += sum(node.displacement .^ 2 .* (node.force - node.oldForce) / Nodes.mass(node))
+
+        cn2 += sum(node.displacement .^ 2)
+    end
+
+    if cn2 != 0.0
+        if cn1 / cn2 > 0.0
+            cn = 2.0 * sqrt(cn1/cn2)
+        end
+    end
+
+    if cn > 2.0
+        cn = 1.9
+    end
+    
+    Threads.@threads for node in nodes
+        # if stale
+        #     velhalf = dt * node.force * 0.5 / Nodes.mass(node) 
+        # else
+        #     velhalf = (2.0 - cn * dt) * node.oldAverageVelocity + 2.0 * dt / Nodes.mass(node)
+        # end
+        averageVelocity = zeros(3)
+        if stale
+            averageVelocity = 0.5 * dt * node.force / node.stableMass
+        else
+            averageVelocity = (2.0 - cn * dt) * node.oldAverageVelocity + 2.0 * (dt / node.stableMass) * (node.force / (2.0 + cn * dt))
+        end
+        node.velocity = 0.5 * (node.oldAverageVelocity + averageVelocity)
+        node.displacement += averageVelocity * dt
+
+        # Displacement BCs
+        for bc in bcs
+            if bc isa BoundaryConditions.DisplacementBoundaryCondition
+                BoundaryConditions.apply_bc(bc)
+            end
+        end
+
+        node.oldAverageVelocity = copy(averageVelocity)
+        node.oldForce = copy(node.force)
+    end
+end
+
+
 end
