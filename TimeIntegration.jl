1cab13a4fb56b9916b9512d2de412b6d4139e90e
watsonkh <watsonkh@vcu.edu>
2022-06-06 18:07:49 -0400

    dynamic integration now uses boundary conditions

--- TimeIntegration.jl
+++ TimeIntegration.jl
@@ -1,20 +1,36 @@
 module TimeIntegration
 
-using ..Materials, ..Nodes, ..Bonds
+using ..Materials, ..Nodes, ..Bonds, ..BoundaryConditions
 
 # function dynamic_stable_timestep()
 
-function dynamic_integration(nodes, bonds, dt)
+function dynamic_integration(nodes, bonds, bcs, dt)
 
     Threads.@threads for node in nodes
         # Average velocity for the time step assuming constant acceleration
         node.velocity = node.velocity + (dt*0.5)*(node.force/(node.volume*node.material.density))
-
+    end
+    
+    # Velocity BCs
+    for bc in bcs
+        if bc isa BoundaryConditions.VelocityBoundaryCondition
+            BoundaryConditions.apply_bc(bc)
+        end
+    end
+    
+    Threads.@threads for node in nodes
         # Update displacement with average velocity
         node.displacement += node.velocity * dt
 
         # Zero out force
         @atomic node.force = zeros(3)
+    end
+
+    # Displacement BCs
+    for bc in bcs
+        if bc isa BoundaryConditions.DisplacementBoundaryCondition
+            BoundaryConditions.apply_bc(bc)
+        end
     end
 
     # Break bonds
