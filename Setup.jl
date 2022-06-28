98b3a9560a972eb3ae4aaa3b32c35ae9184ac2b6
watsonkh <watsonkh@vcu.edu>
2022-06-28 17:35:31 -0400

    setup now sets up conda environement too

--- Setup.jl
+++ Setup.jl
@@ -2,5 +2,10 @@ import Pkg
 Pkg.add("CSV")
 Pkg.add("StaticArrays")
 
+Pkg.add("PyCall")
+PyCall.pyimport_conda("pytables", "pytables")
+PyCall.pyimport_conda("pyvista", "pyvista", "conda-forge")
+PyCall.pyimport_conda("matplotlib.pyplot", "matplotlib", "conda-forge")
+
 # Dont add if using headless:
 # Pkg.add("Plots")
