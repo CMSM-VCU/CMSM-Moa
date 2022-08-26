b7256905a06a759417a0e746fcee861291ad8c1d
watsonkh <watsonkh@vcu.edu>
2022-08-26 15:54:59 -0400

    Initial tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,13 @@ using Moa
 using Test
 
 @testset "Moa.jl" begin
+    # Is the test set connected to Moa?
     @test Moa.HelloWorld()
+
+    state = Moa.parse_input("test/twopoint/twopoint.toml")
+    @test state ≠ Nothing
+    @test length(state.nodes) == 2
+    @test length(state.bonds) == 2
+
+
 end
